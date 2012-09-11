bbbfa54f35e4618fbcb731832083712784a188fa
Michal Seta <mis@artengine.ca>
2012-09-11 01:39:41 -0400

    no real change

--- gst-videofile2videoloopback.sh
+++ gst-videofile2videoloopback.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Remember to modprobe v4l2loopback"
-gst-launch -v filesrc location=$1 ! decodebin ! ffmpegcolorspace ! v4l2sink device=/dev/video0
+gst-launch -v filesrc location=$1 ! decodebin ! ffmpegcolorspace ! v4l2sink device=/dev/video1
